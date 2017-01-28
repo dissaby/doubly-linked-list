d8fd242c4af3bc81eaf7570a4c0a030d0edf8db3
dissaby <dissaby@gmail.com>
2017-01-28 22:03:57 +0300

    5 test passed ok

--- src/linked-list.js
+++ src/linked-list.js
@@ -3,22 +3,35 @@ const Node = require('./node');
 class LinkedList {
 
     constructor() {
-        this.head = null;
-        this.tail = null;
+        this._head = null;
+        this._tail = null;
         this.length = 0;
     }
 
     append(data) {
-        if (this.length = 0) {
-            this.head = data;
-            this.tail = data;
+        var newNode = new Node(data);
+        if (this.length == 0) {
+            this._head = newNode;
+            this._tail = newNode;
+        } else {
+            this._tail.next = newNode;
+            newNode.prev = this._tail;
+            this._tail = newNode;
         }
+        this.length++;
+        return this;
     }
 
     head() {
+        if (this.length != null) {
+            return this._head.data;
+        }
     }
 
     tail() {
+        if (this._tail != null) {
+            return this._tail.data;
+        }
     }
 
     at(index) {
